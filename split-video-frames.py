889c2dee5f54c5b5e096a5a1d11405597f815249
Kevin Scott <kevin@hitheory.com>
2018-12-05 13:41:01 -0500

    Remove logging statements

--- split-video-frames.py
+++ split-video-frames.py
@@ -16,21 +16,15 @@ if not args.target:
     raise Exception('Please provide a target argument')
 
 def separateFrames(source, target):
-    print('sep 1')
     os.makedirs(target, exist_ok=True)
-    print('sep 2')
     cap = cv2.VideoCapture(source)
     success,image = cap.read()
-    print(success, image)
     if success is False:
         raise Exception('Could not read video file')
 
     count = 0
     while success:
         fileTarget = "%s/%d.jpg" % (target, count)
-        # fileTarget = "%d.jpg" % (count)
-        # fileTarget = os.path.join('/pfs/out', os.path("%d.jpg" % (count)))
-        print('file target', fileTarget)
         cv2.imwrite(fileTarget, image)
         success,image = cap.read()
         count += 1
@@ -42,9 +36,5 @@ target = args.target.split(',')
 if len(target) is not len(source):
     raise Exception('Mismatch between source and target args')
 
-for i, s in enumerate(source):
-    file = source[i]
-    # file = os.path.join('/pfs/videos', source[i])
+for i, file in enumerate(source):
     separateFrames(file, target[i])
-
-
